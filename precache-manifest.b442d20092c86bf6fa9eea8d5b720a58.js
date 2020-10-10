5ec54b5602aa262ea27d7a3e191aaed9a923012a
Abner-Hernandez <abner18enero@gmail.com>
2020-10-09 18:51:14 -0600

    Updates

---
+++ precache-manifest.b442d20092c86bf6fa9eea8d5b720a58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7eb7195206b93ba1563c7e125a889b4",
+    "url": "/OLC2_2S2020_PY2_201612101/index.html"
+  },
+  {
+    "revision": "8ac8f43edd20b1a102c5",
+    "url": "/OLC2_2S2020_PY2_201612101/static/css/2.2afc2e4b.chunk.css"
+  },
+  {
+    "revision": "2e4a24e128b7e79d73c2",
+    "url": "/OLC2_2S2020_PY2_201612101/static/css/main.1fc973a9.chunk.css"
+  },
+  {
+    "revision": "8ac8f43edd20b1a102c5",
+    "url": "/OLC2_2S2020_PY2_201612101/static/js/2.1c74c16c.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/OLC2_2S2020_PY2_201612101/static/js/2.1c74c16c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e4a24e128b7e79d73c2",
+    "url": "/OLC2_2S2020_PY2_201612101/static/js/main.c59427a0.chunk.js"
+  },
+  {
+    "revision": "158512be3de653810e19",
+    "url": "/OLC2_2S2020_PY2_201612101/static/js/runtime-main.434f8c27.js"
+  }
+]);
